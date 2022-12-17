8d01379a13b0bdc2b6874a841fa7db43ee72ae72
jhorgiGheovani <jhorgigheovani@gmail.com>
2022-12-17 12:22:32 +0700

    Dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
 function App() {
+  const tittle = 'Welcome to my blog';
+  const likes = 50;
+
   return (
     <div className="App">
       <div className="content">
         <h1> App Component</h1>
+        <h2> Tittle: {tittle}</h2>
+        <p> Liked: {likes}</p>
       </div>
     </div>
   );
