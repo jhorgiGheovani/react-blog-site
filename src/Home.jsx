bddf5e17ee5950b8e39b13e571d6f1d66fd14209
jhorgiGheovani <jhorgigheovani@gmail.com>
2022-12-18 13:59:55 +0700

    #7 handle click event

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 export default function Home() {
+  const handleClick = () => {
+    console.log('Hello, Ninja!');
+  };
+  const handleClickAgain = (nama, e) => {
+    console.log('Hello ' + nama, e.target);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickAgain('mario', e)}>Click Me Again</button>
     </div>
   );
 }
