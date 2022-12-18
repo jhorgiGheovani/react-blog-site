bd185063f0f6e7428b15c6833c65233f02889bd9
jhorgiGheovani <jhorgigheovani@gmail.com>
2022-12-18 17:53:38 +0700

    lists and key

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function Home() {
-  const [name, setName] = useState('Jhorgi');
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName('Luigi');
-    setAge(30);
-  };
+  const [blogs, setBlogs] = useState([
+    { tittle: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { tittle: 'My old website', body: 'lorem ipsum...', author: 'mario', id: 2 },
+    { tittle: 'Build website from scratch', body: 'lorem ipsum...', author: 'mario', id: 3 },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.tittle}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
