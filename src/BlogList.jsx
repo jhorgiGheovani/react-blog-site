f7862bc165679ffcb34c9ae1cd1284bd97c7e8d4
jhorgiGheovani <jhorgigheovani@gmail.com>
2022-12-18 19:34:25 +0700

    #12 imlementing props to search features

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-export default function BlogList({ blogs }) {
+export default function BlogList({ blogs, tittle }) {
   //   const blogs = props.blogs;
   return (
     <div>
+      <h2>{tittle}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.tittle}</h2>
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,12 +4,13 @@ import BlogList from './BlogList';
 export default function Home() {
   const [blogs, setBlogs] = useState([
     { tittle: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { tittle: 'My old website', body: 'lorem ipsum...', author: 'mario', id: 2 },
+    { tittle: 'My old website', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { tittle: 'Build website from scratch', body: 'lorem ipsum...', author: 'mario', id: 3 },
   ]);
   return (
     <div className="home">
-      <BlogList blogs={blogs} />
+      <BlogList blogs={blogs} tittle="All Blogs!" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} tittle="Mario's blogs" />
     </div>
   );
 }
