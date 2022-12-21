b3619d89251b544469accf6cd863195b1d3ccad1
jhorgiGheovani <jhorgigheovani@gmail.com>
2022-12-21 22:11:06 +0700

    create 2 page using Route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 function App() {
   const tittle = 'Welcome to my blog';
   const likes = 50;
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
