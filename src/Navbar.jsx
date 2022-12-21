1f31cb6f688a91b7548b56b5732c8877590aff28
jhorgiGheovani <jhorgigheovani@gmail.com>
2022-12-21 22:23:03 +0700

    router links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <div className="navbar">
       <h1>Jhorgi Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
           style={{
             color: 'white',
             backgroundColor: '#f1356d',
@@ -15,7 +15,7 @@ export default function Navbar() {
           }}
         >
           New Blog
-        </a>
+        </Link>
       </div>
     </div>
   );
