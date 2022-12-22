1e19c115484230f32ff7b04d315e66af2adc3ea0
jhorgiGheovani <jhorgigheovani@gmail.com>
2022-12-22 13:53:30 +0700

    #29 Making a POST Request uploade blog to JSON

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,10 +4,26 @@ export default function Create() {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    });
+  };
   return (
     <div className="create">
       <h2>Add new blogs</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog tittle:</label>
         <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
 
@@ -19,10 +35,8 @@ export default function Create() {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
   );
